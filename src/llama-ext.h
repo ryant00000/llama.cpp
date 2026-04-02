ba2521c6a06c11323b0f22e9ae3d3e4d56e4aa77
Ruben Ortlam <rortlam@redhat.com>
2026-04-02 11:39:07 +0200

    move llama_context_device_memory function to llama-ext.h

--- src/llama-ext.h
+++ src/llama-ext.h
@@ -88,3 +88,9 @@ LLAMA_API int32_t llama_model_n_devices(const struct llama_model * model);
 LLAMA_API ggml_backend_dev_t llama_model_get_device(const struct llama_model * model, int i);
 
 LLAMA_API llama_memory_breakdown llama_get_memory_breakdown(const struct llama_context * ctx);
+
+// Returns the projected memory use (model + context + compute) in bytes
+// for the given device within this context. Returns 0 if the device is not used.
+LLAMA_API uint64_t llama_context_device_memory(
+        const struct llama_context * ctx,
+        ggml_backend_dev_t           device);
